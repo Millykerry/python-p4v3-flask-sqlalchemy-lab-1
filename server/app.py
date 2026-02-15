45be062708e709f30f819ed739f5342462512f19
millykerry <kerryom975@gmail.com>
2026-02-15 19:31:07 +0300

    Complete Flask-SQLAlchemy Lab 1

--- server/app.py
+++ server/app.py
@@ -1,4 +1,3 @@
-# server/app.py
 #!/usr/bin/env python3
 
 from flask import Flask, make_response
@@ -20,7 +19,33 @@ def index():
     body = {'message': 'Flask SQLAlchemy Lab 1'}
     return make_response(body, 200)
 
-# Add views here
+
+@app.route('/earthquakes/<int:id>')
+def earthquake_by_id(id):
+    earthquake = Earthquake.query.filter(Earthquake.id == id).first()
+    
+    if earthquake:
+        body = earthquake.to_dict()
+        status = 200
+    else:
+        body = {'message': f'Earthquake {id} not found.'}
+        status = 404
+    
+    return make_response(body, status)
+
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def earthquakes_by_magnitude(magnitude):
+    quakes = []
+    for earthquake in Earthquake.query.filter(Earthquake.magnitude >= magnitude).all():
+        quakes.append(earthquake.to_dict())
+    
+    body = {
+        'count': len(quakes),
+        'quakes': quakes
+    }
+    
+    return make_response(body, 200)
 
 
 if __name__ == '__main__':
